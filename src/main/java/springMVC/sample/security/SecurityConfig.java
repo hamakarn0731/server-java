1fdc16a0fc99fd9c7b6835edf09a94acd8d34279
yuki_ishida <yuki.ishida.common@gmail.com>
2020-04-28 01:22:10 +0900

    beans.xmlで設定済みだった分

--- src/main/java/springMVC/sample/security/SecurityConfig.java
+++ src/main/java/springMVC/sample/security/SecurityConfig.java
@@ -1,19 +1,13 @@
 package springMVC.sample.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,10 +25,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.logout()
                 .logoutSuccessUrl("/");
-    }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        // テストのためパスワードの暗号化はしない
-        return passwordEncoder;
     }
 }
